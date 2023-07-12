6f677403858bd029968bd0e1a2914e6dc7fa866f
Hermann Witte <hermann.witte@alpha-lab.net>
2023-07-12 09:36:02 +0200

    frontend add booking

---
+++ src/frontend/user-frontend/src/components/admin/booking/AddBookingPage.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="adduser-box">
+    <h2>Add Booking</h2>
+    <form>
+      <div class="user-box">
+        <input type="date" id="date" :value="date" required>
+      </div>
+      <div class="user-box" >
+        <span for="halfday">Full day</span><br>
+        <input type="radio" id="fullDay" name="duration" value="true" style="margin: 0" checked>
+        <br><br>
+        <span for="halfday">Half day</span><br>
+        <input type="radio" id="halfday" name="duration" value="false" style="margin: 0">
+      </div>
+      <a @click="addBooking()">
+        <span></span>
+        Submit
+      </a>
+    </form>
+  </div>
+</template>
+<script>
+
+import axios from "axios";
+
+export default {
+  name: 'AddBookingPasge',
+  data() {
+    return {
+      date: new Date().toISOString().split('T')[0]
+    };
+  },
+  methods: {
+    addBooking: async function  () {
+      console.log(document.getElementById("date").value)
+      try {
+        const response = await axios({
+          method: 'post',
+          url: "/api/v1/booking/register",
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          data: {
+            "date": document.getElementById("date").value,
+            "isFullDay": document.getElementById("fullDay").checked,
+          }
+        });
+
+        if (!response.status == 200) {
+          throw new Error('Request failed');
+        } else {
+          window.location.href = "/";
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  }
+};
+
+</script>
+
+<style>
+
+
+</style>
